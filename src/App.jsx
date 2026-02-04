1e4ce4438c3ae1b78b943585de5f97da464a431e
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-04 17:09:20 +0530

    Initial React blog project (Vite)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hello</h1>
+      <h1>Code step by step</h1>
+    </div>
+  );
+}
+
+export default App;
