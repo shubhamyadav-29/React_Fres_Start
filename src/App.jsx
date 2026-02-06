390948a142501f557cd1c5849fea8905954bd301
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-06 13:42:52 +0530

    What is JSX?

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import Login, {Profile,Setting, Userkey}  from "./UserComponent";
-
+import { createElement } from "react";
+// import Login, {Profile,Setting, Userkey}  from "./UserComponent";
+import Home from "./home";
 function App() {
-  return (
-    <>
-      <h1>Importing and exporting Component</h1>
-      <Login/>
-      <Profile/>
-      <Setting/>
-      <h1>{Userkey}</h1>
-    </>
+  // const a = 'shubham'
+  // return (
+  //   <>
+  //     <h1>{a } Importing and exporting Component</h1>
+  //     <Login/>
+  //     <Profile/>
+  //     <Setting/>
+  //     <h1>{Userkey}</h1>
+  //     <button onClick={()=>alert("hello bro ")}>click</button>
+  //   </>
 
-  );
+  // )
+  
+  return(
+    <><Home/></>
+  )
+ 
 }
 
 export default App;
---
+++ src/home.jsx
@@ -0,0 +1,15 @@
+function Home(){
+    return(
+        <>
+        <ul>
+            <li>Invent new traffic lights</li>
+            <li>Rehearse a movie song</li>
+            <li>Improve the spectrum technology</li>
+        </ul>
+        <button onClick={()=>alert("function called")}>Click me</button>
+
+        </>
+    )
+}
+
+export default Home
