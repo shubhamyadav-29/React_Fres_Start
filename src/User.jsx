fcdc672a02a48aae45ea9a76b6bfe2f841226ded
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-09 21:03:18 +0530

    What is Props in React

--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,13 @@
-function User(){
-    return (
-        <div>
-            <h1>User Component</h1>
-        </div>
-    )
+function User({ user, user2 }) {
+  return (
+    <div>
+      <hr />
+
+      <h1>Name:{user.name}</h1>
+      <h1>Age:{user.age}</h1>
+      <h1>Email:{user.email}</h1>
+    </div>
+  );
 }
 
-
-export default User
-
+export default User;
