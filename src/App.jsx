99bbca010b9132de5fae756cbea729fc00077246
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-11 23:32:14 +0530

    How to handle checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import { useState } from "react";
+import Skills from "./Skills";
 
 function App() {
   const [name,setName] =useState('');
   const [password,setPassword] = useState('');
   const [email,setEmail] = useState('')
   return <div>
-    <h1>Controller Component</h1>
-    <form action="" method="get">
-      <input value={name} type="text" placeholder="Enter Name" onChange={(event)=>setName(event.target.value)}/>
-      <br/> <br/>
-      <input value={password} type="password" placeholder="Enter Password" onChange={(event)=>setPassword(event.target.value)}/>
-      <br/><br/>
-      <input value={email} type="email" placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
-      <br/><br/>
-      <button>submit</button>
-      <button onClick={()=>{setEmail("");setName("");setPassword("");}}>clear</button>
-      <h3>{name}</h3>
-      <h3>{password}</h3>
-      <h3>{email}</h3>
-    </form>
+    <h1>Handle CheckBoxes in React</h1>
+    <Skills/>
+   
   </div>;
 }
 
---
+++ src/Skills.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function Skills(){
+    const [skills,setSkills]=useState([])
+    const handleSkills=(event)=>{
+      console.log(event.target.value, event.target.checked);
+   
+      }
+      
+    }
+    return(
+        <div>
+           <h1>Select Your Skills</h1>
+           <input type="checkbox"  id="php" value="php" onChange={handleSkills}/>
+           <label htmlFor="php">PHP</label>
+           <br /><br />
+           <input type="checkbox"  id="js" value="js" onChange={handleSkills}/>
+           <label htmlFor="js">JS</label>
+           <br /><br />
+           <input type="checkbox"  id="C++" value="C++" onChange={handleSkills}/>
+           <label htmlFor="C++">C++</label>
+           <br /><br />
+           <input type="checkbox"  id="Java" value="Java" onChange={handleSkills}/>
+           <label htmlFor="Java">Java</label>
+           <br /><br />
+
+           <h1>{skills.toString()}</h1>
+        </div>
+    )
+}
+
+export default Skills
