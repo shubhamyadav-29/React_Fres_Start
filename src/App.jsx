da32026efb7125a8e4925e5c807c85a578b6cc2b
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-11 22:53:33 +0530

    Learned what is Controlled Component in Reactjs?

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function App(){
-  const [val,setValue] =useState("")
-  return(
-    <div>
-      <h1>Get Input field Value</h1>
-      <input type="text" value={val} onChange={(event)=> setValue
-      (event.target.value)} placeholder="Enter User Name" />
-      
-      <h1>{val}</h1>
-      <button onClick={()=>setValue("")}>Clear value</button>
-    </div>
-  )
+function App() {
+  const [name,setName] =useState('');
+  const [password,setPassword] = useState('');
+  const [email,setEmail] = useState('')
+  return <div>
+    <h1>Controller Component</h1>
+    <form action="" method="get">
+      <input value={name} type="text" placeholder="Enter Name" onChange={(event)=>setName(event.target.value)}/>
+      <br/> <br/>
+      <input value={password} type="password" placeholder="Enter Password" onChange={(event)=>setPassword(event.target.value)}/>
+      <br/><br/>
+      <input value={email} type="email" placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
+      <br/><br/>
+      <button>submit</button>
+      <button onClick={()=>{setEmail("");setName("");setPassword("");}}>clear</button>
+      <h3>{name}</h3>
+      <h3>{password}</h3>
+      <h3>{email}</h3>
+    </form>
+  </div>;
 }
 
-export default App
+export default App;
