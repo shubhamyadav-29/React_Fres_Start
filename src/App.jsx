81c6137e2dc82ddb1d833ec4ef6e9b4ef268235b
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-12 21:43:06 +0530

    Now moving on the topic how handle radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 function App(){
+  const [gender,setGender] =  useState('male');
   return(
     <div>
       <h1>Handle Radio and DropDown</h1>
       <h4>Select Gender</h4>
-      <input type="radio" name="gender" id="male"/>Male
-      <input type="radio" name="gender" id="female"/>Female
+      <input type="radio"  onChange={(event)=>setGender(event.target.value)} name="gender" value={"male"} checked={gender=='male'} id="male"  />
+      <label htmlFor="male">Male</label>
+      <input type="radio"  onChange={(event)=>setGender(event.target.value)} name="gender" value={"female"} checked={gender=='female'} id="female" />
+       <label htmlFor="female">Female</label>
+       <h2>Select Your Gender:{gender}</h2>
     </div>
   )
 }
