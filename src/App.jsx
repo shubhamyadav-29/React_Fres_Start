f497b0cbcfebd9522c4bbe57e0da1b9251c95093
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-13 19:34:31 +0530

    now we are moving to Loops in jsx with Map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
+
 function App(){
-  const [gender,setGender] =  useState('male');
-  const [city,setCity] = useState('');
+ 
   return(
     <div>
-      <h1>Handle Radio and DropDown</h1>
-      <h4>Select Gender</h4>
-      <input type="radio"  onChange={(event)=>setGender(event.target.value)} name="gender" value={"male"} checked={gender=='male'} id="male"  />
-      <label htmlFor="male">Male</label>
-      <input type="radio"  onChange={(event)=>setGender(event.target.value)} name="gender" value={"female"} checked={gender=='female'} id="female" />
-       <label htmlFor="female">Female</label>
-       <h2>Select Your Gender:{gender}</h2>
-       <br /><br /><br />
-       <h4>Select City</h4>
-       <select onChange={(event)=>setCity(event.target.value)} defaultValue={"Delhi"} >
-        <option value="noida">Noida</option>
-        <option value="Gurgaon">Gurgaon</option>
-        <option value="Delhi">Delhi</option>
-       </select>
-       <h2>Selected City : {city}</h2>
+     <h1>Loop in JSX with Map Function</h1>
     </div>
   )
 }
