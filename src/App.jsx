50faf6d9a5c73b8547d3759e7eeed75cdce9f206
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-03-13 20:35:33 +0530

    small practice set

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import User from "./User";
-
+import Clock from "./Clock.jsx"
 function App() {
-  const userName = ["Shubham", "Anil", "Rajat"];
-  const userData = [
-    {
-      name: "Anil",
-      age: "29",
-      email: "anil@test.com",
-      id: 1,
-    },
-    {
-      name: "shubham",
-      age: "21",
-      email: "shubham@test.com",
-      id: 2,
-    },
-    {
-      name: "Rajat",
-      age: "21",
-      email: "peter@test.com",
-      id: 3,
-    },
-      {
-      name: "Raj",
-      age: "21",
-      email: "peter@test.com",
-      id: 4,
-    },
-  ];
   return (
-    <div>
-      <h1>Reuse Component in Loop</h1>
-      {userData.map((user) => (
-        <div key={user.id} >
-          <User data={user}/>
-        </div>
-      ))}
+    <div className="App">
+      <h1>App Component</h1>
+      <Clock />
     </div>
   );
 }
+
 export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react"
+
 function Clock(){
+    const [time,setTime]=useState(0)
+
+    useEffect(()=>{
+        setInterval(()=>{
+            setTime(new Date().toLocaleTimeString());
+        },1000);
+    },[])
     return(
         <div>
-            <h1>Clock Component</h1>
+            <h1>{time}t</h1>
         </div>
     )
 }
+
+
+export default Clock
