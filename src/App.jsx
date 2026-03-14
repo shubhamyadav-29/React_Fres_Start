b34b71dc5f5df240483d99b943ddecef4a123897
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-03-14 16:10:57 +0530

    clock

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Clock from "./Clock.jsx"
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1>
+      
       <Clock />
     </div>
   );
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,19 +1,18 @@
-import { useEffect } from "react"
+import React, { useEffect, useState } from "react";
 
-function Clock(){
-    const [time,setTime]=useState(0)
-
-    useEffect(()=>{
-        setInterval(()=>{
-            setTime(new Date().toLocaleTimeString());
-        },1000);
-    },[])
-    return(
-        <div>
-            <h1>{time}t</h1>
-        </div>
-    )
+function Clock() {
+  const [time, setTime] = useState(new Date().toLocaleString());
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  });
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>Digital Clock</h1>
+      <h2>{time}</h2>
+    </div>
+  );
 }
 
-
-export default Clock
+export default Clock;
